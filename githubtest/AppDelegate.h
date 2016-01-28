e0b3ac9d8b4215132df5848624ef964bef7941dc
iosDev <syrear@naver.com>
2016-01-28 19:09:58 +0900

    Initial Commit

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by 김민수 on 2016. 1. 28..
+//  Copyright © 2016년 김민수. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by 김민수 on 2016. 1. 28..
+//  Copyright © 2016년 김민수. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
